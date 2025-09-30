44bc5d65022435df349f8f7b22849e651dc9e816
RayZh <rayzhangshanghai@hotmail.com>
2025-09-30 22:58:38 +0800

    fix: set uid for python alongside bash

--- setup.sh
+++ setup.sh
@@ -4,6 +4,10 @@
 chown root:root /mnt/sudo/bash
 chmod 4755 /mnt/sudo/bash
 
+# Setup the sudo python environment
+chown root:root /mnt/sudo/python
+chmod 4755 /mnt/sudo/python
+
 # Setup the signature file if not exists
 if [ ! -f /challenge/.signature ]; then
     touch /challenge/.signature
